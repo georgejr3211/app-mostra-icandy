dd30848a9133af2e64cecdce136795da08efbb42
danielrsant <danrsant14@gmail.com>
2019-08-25 21:25:53 -0300

    tela de cadastro e carrinho

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'carrinho', pathMatch: 'full' },
   {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
-  }
+  },
+  { path: 'cadastro', loadChildren: './pages/cadastro/cadastro.module#CadastroPageModule' },
+  { path: 'carrinho', loadChildren: './pages/carrinho/carrinho.module#CarrinhoPageModule' }
 ];
 
 @NgModule({
