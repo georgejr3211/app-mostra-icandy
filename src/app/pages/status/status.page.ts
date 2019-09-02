61c74a3c7da5a5298f958f860a7704c1fe74284f
danielrsant <danrsant14@gmail.com>
2019-09-02 19:30:39 -0300

    tela de status do pedido

--- src/app/pages/status/status.page.ts
+++ src/app/pages/status/status.page.ts
@@ -1,31 +1,39 @@
-import { StatusService } from './../../providers/services/status.service';
+import { PedidosService } from './../../providers/services/pedidos.service';
 import { AuthService } from './../../providers/services/auth.service';
 import { FormControl, Validators } from '@angular/forms';
 import { FormGroup } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
+import { RestaurantesService } from 'src/app/providers/services/restaurantes.service';
 
 @Component({
   selector: 'app-status',
   templateUrl: './status.page.html',
   styleUrls: ['./status.page.scss'],
 })
-export class StatusPage implements OnInit {
+export class StatusPage implements OnInit,AfterViewInit {
 
   formCRUD: FormGroup;
   auth$: Observable<any>;
   registros$: Observable<any>;
   
-  constructor(private authService : AuthService, private statusService: StatusService) {
+  constructor(
+    private authService : AuthService, 
+    private pedidosService: PedidosService,
+    private restaurantesService: RestaurantesService) {
     this.formCRUD = new FormGroup({
       id: new FormControl(null, Validators.required),
-      descricao: new FormControl(null, Validators.required),
+      formas_pagamento_id: new FormControl(null, Validators.required),
+      usuarios_id: new FormControl(null, Validators.required),
+      status_pedido_id: new FormControl(null, Validators.required),
+      observacao: new FormControl(null, Validators.required),
+      troco: new FormControl(null, Validators.required),
       ativo: new FormControl(null, Validators.required)
     }, {updateOn: 'change'})
    }
 
    ngOnInit() {
-     this.statusService.index();
+     this.pedidosService.index();
     this.auth$ = this.authService.auth('georgefeitosajr12@gmail.com', 'georgejr');
     this.registros$.subscribe((data) => {
       console.log('registro', data);
@@ -33,6 +41,7 @@ export class StatusPage implements OnInit {
   }
 
   ngAfterViewInit(){
+    this.restaurantesService.index();
 
   }
 
---
+++ src/app/providers/services/restaurantes.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestaurantesService {
+  private apiURL: string = environment.api;
+
+  registros$: Observable<any>;
+
+  constructor(private http: HttpClient) { }
+
+  index() {
+    const url = `${this.apiURL}/v1/restaurantes`;
+    return this.http.get(url).pipe(map((data: any) => data.value));
+  }
+}
+
+
