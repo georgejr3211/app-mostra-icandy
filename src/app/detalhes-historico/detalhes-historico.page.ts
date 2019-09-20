4874ffbdb6d2a005be4e48c1aa50494e15680cb4
danielrsant <danrsant14@gmail.com>
2019-09-19 21:56:25 -0300

    .

--- src/app/detalhes-historico/detalhes-historico.page.ts
+++ src/app/detalhes-historico/detalhes-historico.page.ts
@@ -1,34 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { PopoverController, NavParams } from '@ionic/angular';
-import { Observable } from 'rxjs';
-import { PedidosProdutosService } from '../providers/services/pedidos-produtos.service';
+import { Component, OnInit } from "@angular/core";
+import { PopoverController, NavParams } from "@ionic/angular";
+import { Observable } from "rxjs";
+import { PedidosProdutosService } from "../providers/services/pedidos-produtos.service";
 
 @Component({
-  selector: 'app-detalhes-historico',
-  templateUrl: './detalhes-historico.page.html',
-  styleUrls: ['./detalhes-historico.page.scss'],
+  selector: "app-detalhes-historico",
+  templateUrl: "./detalhes-historico.page.html",
+  styleUrls: ["./detalhes-historico.page.scss"]
 })
 export class DetalhesHistoricoPage implements OnInit {
-
-  passedId =  null;
+  passedId = null;
   pedido$: Observable<any>;
+  quantidade = 0;
+  preco: number;
 
   constructor(
-    private facade: PedidosProdutosService, 
+    private facade: PedidosProdutosService,
     public popoverController: PopoverController,
     private navParams: NavParams
   ) {
-    this.passedId = this.navParams.get('custom_id');
-    console.log('AOBA', this.passedId);
-   }
+    this.passedId = this.navParams.get("custom_id");
+    console.log("AOBA", this.passedId);
+  }
 
   ngOnInit() {
     this.pedido$ = this.facade.findPedido(this.passedId);
-    this.pedido$.subscribe(data => console.log('data', data));
+    this.pedido$.subscribe(data => {
+      data.map(element => {
+        this.preco = parseInt(element.produto.preco);
+        this.quantidade = this.quantidade + this.preco;
+        console.log(this.quantidade);
+      })
+    });
   }
 
   dismiss() {
     this.popoverController.dismiss();
   }
-
 }
