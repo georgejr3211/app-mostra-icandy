1266586213a3617994b71a0fbc8484912f267656
George Alexandre <georgefeitosajr12@gmail.com>
2019-09-20 20:37:32 -0300

    gerando apk

---
+++ src/app/providers/services/notification.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+// import { AngularFirestore } from 'angularfire2/firestore';
+// import {Firebase} from '@ionic-native/firebase/ngx';
+import { Platform } from '@ionic/angular';
+
+@Injectable()
+export class NotificationsService {
+
+}
