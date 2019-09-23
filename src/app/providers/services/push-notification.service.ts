210c11daa103f00424a582a9f7ee432704a036fc
George Alexandre <georgefeitosajr12@gmail.com>
2019-09-23 12:42:29 -0300

    Adicionando o push notification

--- src/app/providers/services/push-notification.service.ts
+++ src/app/providers/services/push-notification.service.ts
@@ -10,18 +10,15 @@ export class PushNotificationService {
   constructor(private oneSignal: OneSignal) { }
 
   init() {
-    this.oneSignal.startInit('80472705-93cd-4b34-a70c-6eff60c736bb', '26412989148');
-    console.log('init');
+    this.oneSignal.startInit('71f90043-e77c-4037-a62a-bcce5f50e60d', '26412989148');
     this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
 
     this.oneSignal.handleNotificationReceived().subscribe(() => {
       // do something when notification is received
-      console.log('testando');
     });
 
     this.oneSignal.handleNotificationOpened().subscribe(() => {
       // do something when a notification is opened
-      console.log('testando 2');
 
     });
 
