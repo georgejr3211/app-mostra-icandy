554c49a158ca16dfbfc03394e4bc16e147422f44
danielrsant <danrsant14@gmail.com>
2019-10-11 20:07:41 -0300

    adicionando cpf no carrinho

--- src/app/pages/carrinho/carrinho.page.ts
+++ src/app/pages/carrinho/carrinho.page.ts
@@ -1,18 +1,18 @@
-import { ModalController, NavController } from '@ionic/angular';
+import { ModalController, NavController } from "@ionic/angular";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { Component, OnInit, ViewChild, Injectable } from "@angular/core";
-import { Observable } from 'rxjs/internal/Observable';
-import { CarrinhoCompraService } from '../../providers/services/carrinho-compra.service';
-import { map } from 'rxjs/operators';
-import { PedidosService } from 'src/app/providers/services/pedidos.service';
-import { AlertController } from '@ionic/angular';
-import { Router } from '@angular/router';
-import { LocalizacoesService } from '../../providers/services/localizacoes.service';
-import { FormasPagamentoService } from 'src/app/providers/services/formas-pagamento.service';
-import { HomePage } from '../home/home.page';
-import { PushNotificationService } from 'src/app/providers/services/push-notification.service';
-import { UsuariosService } from 'src/app/providers/services/usuarios.service';
-import { LocalEntregaPage } from '../local-entrega/local-entrega.page';
+import { Observable } from "rxjs/internal/Observable";
+import { CarrinhoCompraService } from "../../providers/services/carrinho-compra.service";
+import { map } from "rxjs/operators";
+import { PedidosService } from "src/app/providers/services/pedidos.service";
+import { AlertController } from "@ionic/angular";
+import { Router } from "@angular/router";
+import { LocalizacoesService } from "../../providers/services/localizacoes.service";
+import { FormasPagamentoService } from "src/app/providers/services/formas-pagamento.service";
+import { HomePage } from "../home/home.page";
+import { PushNotificationService } from "src/app/providers/services/push-notification.service";
+import { UsuariosService } from "src/app/providers/services/usuarios.service";
+import { LocalEntregaPage } from "../local-entrega/local-entrega.page";
 
 @Component({
   selector: "app-carrinho",
@@ -20,16 +20,29 @@ import { LocalEntregaPage } from '../local-entrega/local-entrega.page';
   styleUrls: ["./carrinho.page.scss"]
 })
 export class CarrinhoPage implements OnInit {
+  validation_messages = {
+    cpf: [
+      {
+        type: "minlength",
+        message: "O CPF deve ter 11 caracteres."
+      }
+    ]
+  };
+
+  formCRUD: FormGroup;
+  formCRUDCPF: FormGroup;
 
   auth$: Observable<string>;
-  formCRUD: FormGroup;
+  usuario$: Observable<any>;
   produtoCarrinho$: Observable<any[]>;
   localizacoes$: Observable<any[]>;
   formasPagamento$: Observable<any[]>;
+
+  cpf: string;
   totalCompra: number;
   disabled: boolean;
   adminsDevices = [];
-  
+
   constructor(
     private carrinhoCompraService: CarrinhoCompraService,
     private pedidoService: PedidosService,
@@ -41,9 +54,8 @@ export class CarrinhoPage implements OnInit {
     private usuario: UsuariosService,
     private modalCtrl: ModalController,
     private nav: NavController
-    ) {
-      
-      this.formCRUD = new FormGroup(
+  ) {
+    this.formCRUD = new FormGroup(
       {
         id: new FormControl(null, {}),
         formas_pagamento_id: new FormControl(1, Validators.required),
@@ -51,20 +63,32 @@ export class CarrinhoPage implements OnInit {
         troco: new FormControl({ value: null, disabled: true }),
         itens: new FormControl(null, Validators.required),
         localEntrega: new FormControl(3, Validators.required),
-        valorTotal: new FormControl(null),
+        valorTotal: new FormControl(null)
       },
       { updateOn: "change" }
     );
 
+    this.formCRUDCPF = new FormGroup(
+      {
+        cpf: new FormControl(
+          null,
+          Validators.compose([Validators.minLength(14)])
+        )
+      },
+      { updateOn: "change" }
+    );
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   ionViewDidEnter() {
     this.usuario.indexAdminDevices().subscribe(data => {
-      if (!data) {return;}
-      this.adminsDevices = data.filter(user => user.device_id).map(user => user.device_id);
+      if (!data) {
+        return;
+      }
+      this.adminsDevices = data
+        .filter(user => user.device_id)
+        .map(user => user.device_id);
     });
     this.onRefresh();
   }
@@ -72,7 +96,8 @@ export class CarrinhoPage implements OnInit {
   onRefresh() {
     this.localizacoes$ = this.localizacoesService.index();
     this.formasPagamento$ = this.formasPagamentoService.index();
-    this.produtoCarrinho$ = this.carrinhoCompraService.getProdutosCarrinho()
+    this.produtoCarrinho$ = this.carrinhoCompraService
+      .getProdutosCarrinho()
       .pipe(
         map((data: any) => {
           if (!data) {
@@ -84,8 +109,8 @@ export class CarrinhoPage implements OnInit {
             .reduce((a, b) => a + b, 0)
             .toFixed(2);
 
-          this.formCRUD.get('itens').setValue(data.carrinho);
-          this.formCRUD.get('valorTotal').setValue(this.totalCompra);
+          this.formCRUD.get("itens").setValue(data.carrinho);
+          this.formCRUD.get("valorTotal").setValue(this.totalCompra);
 
           return data.carrinho;
         })
@@ -95,32 +120,45 @@ export class CarrinhoPage implements OnInit {
   hasTroco(value) {
     const disabled = !value.detail.checked;
     if (disabled) {
-      this.formCRUD.get('troco').setValue(null)
-      this.formCRUD.get('troco').disable();
+      this.formCRUD.get("troco").setValue(null);
+      this.formCRUD.get("troco").disable();
     } else {
-      this.formCRUD.get('troco').enable();
+      this.formCRUD.get("troco").enable();
     }
   }
 
   createPedido() {
-    const data = this.formCRUD.value;
-    this.pedidoService.insert(data).subscribe(data => {
-      localStorage.setItem('id-ultimo-pedido', data.id);
-      this.router.navigate([`./main/status/${data.id}`]);
-    });
-    this.push.sendMessageToAdmins(this.adminsDevices, 'Um novo pedido foi realizado!');
-    this.carrinhoCompraService.addProdutoCarrinho({ carrinho: [], qtd: 0 });
+    if (this.formCRUDCPF.value) {
+      console.log('entrou');
+      this.usuario$ = this.usuario.usuarioLogado();
+      this.usuario$.subscribe(data => {
+        if (data) {
+          this.formatCpf();
+          console.log(data.id, this.formCRUDCPF.get('cpf').value);
+          this.usuario.update({id: data.id, cpf: this.formCRUDCPF.get('cpf').value }).subscribe(data => console.log('data',data));
+        }
+      });
+    }
+    // const data = this.formCRUD.value;
+    // this.pedidoService.insert(data).subscribe(data => {
+    //   localStorage.setItem("id-ultimo-pedido", data.id);
+    //   this.router.navigate([`./main/status/${data.id}`]);
+    // });
+    // this.push.sendMessageToAdmins(
+    //   this.adminsDevices,
+    //   "Um novo pedido foi realizado!"
+    // );
+    // this.carrinhoCompraService.addProdutoCarrinho({ carrinho: [], qtd: 0 });
   }
 
-  updateStatus() {
-    this.pedidoService.update({
-      id: 14,
-      status_pedido_id: 1 + Math.floor(Math.random() * Math.floor(4))
-    }).subscribe();
+  formatCpf() {
+    this.cpf = this.formCRUDCPF.get("cpf").value;
+    this.cpf = this.cpf.replace(/-/g, "");
+    this.cpf = this.cpf.replace(/[{(.)}]/g, "");
+    this.formCRUDCPF.get("cpf").setValue(this.cpf);
   }
 
   escolherLocalEntrega() {
-    this.nav.navigateForward('/main/local-entrega');
+    this.nav.navigateForward("/main/local-entrega");
   }
-
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
 
-  // api: 'http://192.168.0.20:3000'
+  // api: 'http://localhost:3000'
   api: 'http://api-icandy-com-br.umbler.net'
 };
