1ea2a6f7cb264590608e08cfa45d9e464df5b7fb
danielrsant <danrsant14@gmail.com>
2019-10-13 23:33:27 -0300

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
 
 
-  api: 'http://192.168.1.105:4000'
-  // api: 'http://api-icandy-com-br.umbler.net'
+  // api: 'http://192.168.1.105:4000'
+  api: 'http://api-icandy-com-br.umbler.net'
   // api: 'https://icandybr.herokuapp.com'
 };
