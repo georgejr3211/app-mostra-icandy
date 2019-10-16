15504f07bc378a53f4ccfd1ddfa7b8e92623021d
George Alexandre <georgefeitosajr12@gmail.com>
2019-10-16 19:29:54 -0300

    ajeitando camera service para funcionar em produtos e usuarios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
 
   // api: 'http://192.168.43.40:4000'
-  // api: 'http://api-icandy-com-br.umbler.net'
-  api: 'https://icandybr.herokuapp.com'
+  api: 'http://api-icandy-com-br.umbler.net'
+  // api: 'https://icandybr.herokuapp.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
 
 
   // api: 'http://192.168.1.105:4000'
-  // api: 'http://api-icandy-com-br.umbler.net'
+  api: 'http://api-icandy-com-br.umbler.net'
   // api: 'http://192.168.100.11:3000'
-  api: 'https://icandybr.herokuapp.com'
+  // api: 'https://icandybr.herokuapp.com'
 };
