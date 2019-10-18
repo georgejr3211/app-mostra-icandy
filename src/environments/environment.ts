24381bec0dfd2eb9e8e81697dbc48c9d771c91eb
George Alexandre <georgefeitosajr12@gmail.com>
2019-10-18 20:44:50 -0300

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
 
 
-  api: 'http://172.20.10.6:3000'
+  api: 'https://icandybr.herokuapp.com'
   // api: 'http://api-icandy-com-br.umbler.net'
   // api: 'http://192.168.0.21:3000'
   // api: 'https://icandybr.herokuapp.com'
