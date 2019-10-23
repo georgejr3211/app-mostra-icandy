80c80f9959b7b165eab0696ff333669f8fd834c0
George Alexandre <georgefeitosajr12@gmail.com>
2019-10-22 23:32:56 -0300

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
 
   // api: 'http://192.168.0.21:3000'
   // api: 'https://icandybr.herokuapp.com'
-  api: 'http://api-icandy-com-br.umbler.net'
+  // api: 'http://api-icandy-com-br.umbler.net'
   // api: 'https://icandybr.herokuapp.com'
+  api: 'https://api-icandy.xyz'
 };
